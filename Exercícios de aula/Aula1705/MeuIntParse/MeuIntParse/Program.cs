4871011cf856e5afdf6507081117060628525061
Jéssica Ribeiro <jessicafirib@gmail.com>
2017-05-17 15:28:12 -0300

    Exercício Meu intParse parte 1.

---
+++ Exercícios de aula/Aula1705/MeuIntParse/MeuIntParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuIntParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 * MeuIntParse(Console.ReadLine()));
+        }
+
+        public static int MeuIntParse(string nroComoStr)
+        {
+
+        }
+    }
+}
