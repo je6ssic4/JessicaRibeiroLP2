40795611420efdc8ede7f5a66c00d475e62565ce
Jéssica Ribeiro <jessicafirib@gmail.com>
2017-05-17 15:06:33 -0300

    Exercício de contagem de vogais.

---
+++ Exercícios de aula/Aula1705/ContagemDeVogais/ContagemDeVogais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContagemDeVogais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            int vogal = 0;
+
+            Console.WriteLine("Digite o texto:");
+            texto = Console.ReadLine();
+
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i] == 'a' || texto[i] == 'e' || texto[i] == 'i' || texto[i] == 'o' || texto[i] == 'u' || texto[i] == 'A' || texto[i] == 'E' || texto[i] == 'I' || texto[i] == 'O' || texto[i] == 'U')
+                {
+                    vogal++;
+                }
+            }
+            Console.WriteLine("O texto tem {0} vogais", vogal);
+        }
+
+    }
+}
