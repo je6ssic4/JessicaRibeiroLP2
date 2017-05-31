a9aff20ace80fcb84c84be1f728e0122cef2bb87
je6ssic4 <jessicafirib@gmail.com>
2017-05-31 15:40:16 -0300

    Progama ObjetosNoPlano parte 1.

---
+++ Exercícios de aula/Aula3105/ObjetosNoPlano/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 600, L = 800;
+
+            Console.WriteLine("Digite X inicial:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite Y inicial:");
+            int y = int.Parse(Console.ReadLine());
+
+            Objeto2D obj = new Objeto2D();
+
+
+            Console.WriteLine("Digite um comando: ");
+            ConsoleKey comando = Console.ReadKey().Key;
+
+            if (comando == ConsoleKey.RightArrow)
+            {
+                obj.AndarParaADireita();
+                Console.WriteLine(obj.Coordenadas());
+            }
+                
+
+        }
+    }
+}
