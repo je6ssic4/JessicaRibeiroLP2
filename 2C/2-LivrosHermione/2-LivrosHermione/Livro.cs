0d78ba65d2d70ee6803eecda9ef7e155daaa37a8
Jéssica Ribeiro Filho <jessicafirib@gmail.com>
2017-07-05 16:01:13 -0300

    Livros da Hermione incompleto

---
+++ 2C/2-LivrosHermione/2-LivrosHermione/Livro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_LivrosHermione
+{
+    class Livro
+    {
+        public string titulo;
+        public int qtdPag, pagPoH, HoraPorDia, diasLendo, pagPorDia;
+
+        public double TempLeitura(int pagPoH)
+        {
+            pagPorDia = pagPoH * HoraPorDia;
+            double diasLendo = qtdPag / pagPorDia;
+
+            return diasLendo; 
+
+        }
+    }
+}
