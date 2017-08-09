a0339b1218d513ab881c0f47549174c532a91127
Jéssica Ribeiro <jessicafirib@gmail.com>
2017-08-09 15:58:33 -0300

    Exercício do Código Morse incompleto

---
+++ 2C/4-CodigoMorse/4-CodigoMorse/4-CodigoMorse/Mensagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class Mensagem
+    {
+        public string texto;
+        public string tabela { get; }
+
+        public string Codificar()
+        {
+            Console.WriteLine("Digite o texto:");
+            texto = Console.ReadLine();
+
+            return texto;
+        }
+
+    }
+}
---
+++ 2C/4-CodigoMorse/4-CodigoMorse/4-CodigoMorse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_CodigoMorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int com;
+            string x;
+
+            Console.WriteLine("Digite o texto");
+            x = Console.ReadLine();
+
+            Console.WriteLine("Digite o comando:");
+            com = int.Parse(Console.ReadLine());
+
+            string[] tabela = new string[] {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+            string [] letras = new string[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","Q","R","S","T","U","V","W","X","Y","Z"};
+
+            for(int i = 1; i <= x.Length; i++)
+            {
+                int n = x[i] - i;
+
+                tabela[n] = letras[n];
+            }
+         
+        }
+    }
+}
