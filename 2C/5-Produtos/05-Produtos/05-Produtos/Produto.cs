8da84bf968d28b8272e30fec1278d9d623692a36
Jéssica Ribeiro <jessicafirib@gmail.com>
2017-08-16 16:11:02 -0300

    Classe

---
+++ 2C/5-Produtos/05-Produtos/05-Produtos/Produto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Produto
+    {
+        private int id;
+        public int Id {
+            get { return id; }
+        }
+        private string nome;
+        public string Nome {
+            get { return nome; }
+        }
+        private int quantidade;
+        public int Quantidade { 
+            get { return quantidade; }
+        }
+        public double Preco { get; set; }
+
+        public void Retirada(int qtd)
+        {
+            if (qtd <= this.quantidade)
+            {
+                this.quantidade -= qtd;
+            }
+            else
+                throw new Exception("Quantidade supera estoque.");
+        }
+        public void Reposicao(int qtd)
+        {
+                quantidade += qtd;
+        }
+        public Produto(int id, string nome, double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.Preco = preco;
+            this.quantidade = 0;
+        }
+        public string Imprimir()
+        {
+            return string.Format("Produto:{0}, {1} - R${2:0.00}", id, nome, Preco);
+        }
+ 
+   
+    }
+
+}
---
+++ 2C/5-Produtos/05-Produtos/05-Produtos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto(1, "Sabonete Francis 60g", 1.49);
+            p.Reposicao(100);
+            p.Retirada(20);
+
+            Console.WriteLine(p.Imprimir());
+            p.Retirada(90);
+        }
+    }
+}
