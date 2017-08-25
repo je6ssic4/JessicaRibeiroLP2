ea29fad3ed199427bf5c74ee7ad076a5cc9986c7
Jéssica Ribeiro Filho <jessicafirib@gmail.com>
2017-08-25 14:40:50 -0300

    Atualização e exercício Geometria parte 1.

---
+++ 2C/06-Geometria/06-Geometria/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Geometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Triangulo a = new Triangulo();
+
+            Console.WriteLine("Qual a base do triângulo?");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual a altura do triângulo?");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do triângulo é de:{0}", a.Area());
+
+        }
+    }
+}
---
+++ 2C/06-Geometria/06-Geometria/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Geometria
+{
+    class Triangulo
+    {
+        private int B; {get; set;}
+        private int H; {get; set;}
+        public int Area(int B, int H)
+        {
+            int t;
+            
+            t = (B * H) / 2;
+
+            return t;
+        }
+    }
+}
