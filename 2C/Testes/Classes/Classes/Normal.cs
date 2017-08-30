56199179d24db355bdbfe85d02b2621ccba08c0b
Jéssica Ribeiro Filho <jessicafirib@gmail.com>
2017-08-30 17:55:50 -0300

    Testes de Classes e exercício 07-ConversorMedidas.

---
+++ 2C/Testes/Classes/Classes/Normal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Normal
+    {
+        public string Nome { get; set; }
+        public int Number { get; set; }
+
+        public void Soma()
+        {
+            this.Number = Number + 3;
+        }
+        public void Multi()
+        {
+            this.Number = Number * 3;
+        }
+    }
+}
---
+++ 2C/Testes/Classes/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Normal pessoa = new Normal();
+            pessoa.Nome = "Jéssica";
+            pessoa.Number = 11;
+
+            Console.WriteLine("{0}, {1}", pessoa.Nome, pessoa.Number);
+        }
+    }
+}
